1d100b231fcb2444c9d54b2aa485b72bb3ec8f02
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 20:52:04 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   levitation_force = 6
 	
   while levitation_force < log10
-    puts "Hello number #{levitation_force}."
+    puts "Wingardium Leviosa"
     levitation_force += 1 
   end
 end
